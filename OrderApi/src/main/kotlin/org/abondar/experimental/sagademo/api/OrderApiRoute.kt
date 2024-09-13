f75e4ee4fdd1a53fceb9eff4a3efed1762776db7
Alex Bondar <abondar1992@gmail.com>
2024-09-13 19:10:33 +0200

    clean api route

--- OrderApi/src/main/kotlin/org/abondar/experimental/sagademo/api/OrderApiRoute.kt
+++ OrderApi/src/main/kotlin/org/abondar/experimental/sagademo/api/OrderApiRoute.kt
@@ -1,7 +1,6 @@
 package org.abondar.experimental.sagademo.api
 
 import org.abondar.experimental.sagademo.message.OrderCreateRequest
-import org.abondar.experimental.sagademo.message.OrderMessage
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.model.rest.RestBindingMode
 import org.springframework.stereotype.Component
@@ -17,8 +16,8 @@ class OrderApiRoute : RouteBuilder() {
             .enableCORS(true)
             .contextPath("/api")
             .apiContextPath("/api-doc")
-            .apiProperty("api.title","Order Saga API")
-            .apiProperty("api.version","1")
+            .apiProperty("api.title", "Order Saga API")
+            .apiProperty("api.version", "1")
             .component("servlet")
             .bindingMode(RestBindingMode.auto)
 
@@ -34,8 +33,6 @@ class OrderApiRoute : RouteBuilder() {
 
             .delete("/{orderId}")
             .routeId("orderDelete")
-            .consumes("application/json")
-            .produces("application/json")
             .apiDocs(true)
             .to("jms:queue:cancelOrderProcessing")
     }
