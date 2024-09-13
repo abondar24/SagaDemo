4594dc9df0a39394d15db8f2b2c2253c9b9b0a29
Alex Bondar <abondar1992@gmail.com>
2024-09-13 14:49:50 +0200

    gradle refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,6 @@ subprojects {
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.apache.camel.springboot:camel-spring-boot-starter:4.7.0")
         implementation("org.apache.camel.springboot:camel-jms-starter:4.7.0")
-        implementation("org.apache.camel.springboot:camel-lra-starter:4.7.0")
         implementation("org.flywaydb:flyway-core:10.15.0")
 
         runtimeOnly("com.h2database:h2")
